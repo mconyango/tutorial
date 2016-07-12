622d357af8d3502341e703bcd977097b5c7043aa
mconyango <mconyango@gmail.com>
2016-07-12 12:34:41 +0300

    added db dump

---
+++ _protected/common/config/db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Fred <mconyango@gmail.com>
+ * Date: 2016/01/11
+ * Time: 3:58 PM
+ */
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=tutorial',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
